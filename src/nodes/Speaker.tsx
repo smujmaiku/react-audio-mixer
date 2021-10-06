5d316d251a10e4f3f67abbe47e7a30a23305d09c
Michael Szmadzinski <smujmaiku@gmail.com>
2021-10-05 21:35:25 -0400

    reconnect speaker audio when device is recovered

--- src/nodes/Speaker.tsx
+++ src/nodes/Speaker.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo } from 'react';
 import useAudio, { BaseOutNodeProps, CustomNode } from '../audioContext';
 import { useOutputDevices } from '../hooks/devices';
+import useErrorCallback from '../hooks/errorCallback'
 
 declare global {
 	// Add sinkId for Chrome
@@ -16,15 +17,19 @@ export const canSinkId = ('sinkId' in HTMLMediaElement.prototype);
 
 export interface SpeakerNodeProps extends BaseOutNodeProps {
 	deviceId?: string;
+	onAudio: (audio: HTMLAudioElement) => void;
 }
 
 export default function SpeakerNode(props: SpeakerNodeProps): JSX.Element | null {
 	const {
 		deviceId,
+		onAudio,
 		onError,
 		...baseNodeProps
 	} = props;
 	const { context, ready } = useAudio();
+
+	const handleError = useErrorCallback(onError);
 
 	const [devices] = useOutputDevices();
 	const isDeviceKnown = !deviceId || devices.some((row) => row.deviceId === deviceId);
@@ -33,20 +38,35 @@ export default function SpeakerNode(props: SpeakerNodeProps): JSX.Element | null
 		const audio = new Audio();
 		audio.muted = true;
 		return audio;
-	}, []);
+		// Reload audio element when device is recovered
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isDeviceKnown]);
+
+	useEffect(() => {
+		if (!onAudio) return;
+		onAudio(audio);
+	}, [audio, onAudio]);
+
+	useEffect(() => () => {
+		audio.muted = true;
+		audio.srcObject = null;
+		audio.pause();
+	}, [audio]);
+
+	useEffect(() => {
+		if (!ready) return;
+		audio.play();
+	}, [audio, ready]);
 
 	const node = useMemo(() => {
 		try {
 			const node = context.createMediaStreamDestination();
 			audio.srcObject = node.stream;
 			return node;
-		} catch (e) { }
-	}, [context, audio]);
-
-	useEffect(() => {
-		if (!ready) return;
-		audio.play();
-	}, [audio, ready]);
+		} catch (error) {
+			handleError(error);
+		}
+	}, [context, audio, handleError]);
 
 	useEffect(() => {
 		if (!audio || !ready) return;
@@ -65,7 +85,7 @@ export default function SpeakerNode(props: SpeakerNodeProps): JSX.Element | null
 					await audio.setSinkId(deviceId);
 				}
 			} catch (error) {
-				onError?.(error);
+				handleError(error);
 			}
 
 			if (cancel) return;
@@ -75,7 +95,7 @@ export default function SpeakerNode(props: SpeakerNodeProps): JSX.Element | null
 		if (canSinkId) {
 			setBySinkId();
 		} else if (deviceId) {
-			onError?.(new Error(SETDEVICEID_ERROR_MSG))
+			handleError(new Error(SETDEVICEID_ERROR_MSG))
 		} else {
 			audio.muted = false;
 		}
@@ -83,7 +103,7 @@ export default function SpeakerNode(props: SpeakerNodeProps): JSX.Element | null
 		return () => {
 			cancel = true;
 		};
-	}, [audio, node, deviceId, ready, isDeviceKnown, onError]);
+	}, [audio, node, deviceId, ready, isDeviceKnown, handleError]);
 
 	if (!node) return null;
 
